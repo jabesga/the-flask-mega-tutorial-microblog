e8461a2eb891237d1f9596837515f3684b5f725b
Jon Ander Besga <jonan.bsg@gmail.com>
2016-07-30 18:54:02 +0200

    added login form

---
+++ config.py
@@ -0,0 +1,9 @@
+WTF_CSRF_ENABLED = True
+SECRET_KEY = '_s1=ut&^%2d)1^$p-$s4-sh1^*-@x8a446p@5e9gc^s@68rjs1'
+
+OPENID_PROVIDERS = [
+    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
