b1c121b1e2f17bdca8db3847046d6db9e8874e66
Jon Ander Besga <jonan.bsg@gmail.com>
2016-07-30 18:54:02 +0200

    added login form

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
 @app.route('/')
 def index():
@@ -18,3 +19,15 @@ def index():
                            title='Home',
                            user=user,
                            posts=posts)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash_message = 'Login requested for OpenID="{}", remember_me={}'.format(form.openid.data, form.remember_me.data)
+        flash(flash_message)
+        return redirect('/')
+    return render_template('login.html',
+                           title='Sign In',
+                           form=form,
+                           providers=app.config['OPENID_PROVIDERS'])
