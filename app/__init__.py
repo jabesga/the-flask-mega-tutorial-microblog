895d0af62bea33c82ced8447276464bf3ed01c7d
Jon Ander Besga <jonan.bsg@gmail.com>
2016-07-31 16:34:56 +0200

    OAuth with Twitter and Facebook working. Still remains to implement Flask Login

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+from flask_oauthlib.client import OAuth, OAuthException
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -10,5 +12,10 @@ migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+
+oauth = OAuth(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 from app import views, models
